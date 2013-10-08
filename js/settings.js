dfee5db78f49b9f00a3f6db25ce6807858b06815
Mikko Johansson <mikko.johansson@gmail.com>
2013-10-08 16:37:27 +0300

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -15,7 +15,7 @@ var defaults = {
   "rootUrl": "http://localhost:8000/api/api/v1/",
   "projection": "EPSG:900913",
   "projection2": "EPSG:4326",
-  "bingKey": "AjiRFAOxAb5Z01PMW3EwdUrCjDhN88QKPA3OfFmUuheW4ByTUZ9XPySvAv50RUpR"
+  "bingKey": "<REMOVED>"
 }
 
 var categorySettings = {
